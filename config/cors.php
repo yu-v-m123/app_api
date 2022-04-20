986f992cb20815d71d7909cc06a93e99ef2e73b4
yu-v-m123 <nagase.yuki0123@gmail.com>
2022-04-21 01:29:24 +0900

    [fix]corsのpath修正

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'login', 'register', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
