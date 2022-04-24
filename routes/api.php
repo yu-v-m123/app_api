01d5d2d4dfb88c3b20f751136d4124bba82a95b1
yu-v-m123 <nagase.yuki0123@gmail.com>
2022-04-24 18:24:31 +0900

    [fix]use declaration

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Api\Auth\LoginController;
-use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Admin\UserController;
 
 /*
@@ -17,8 +16,8 @@ use App\Http\Controllers\Api\Admin\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
